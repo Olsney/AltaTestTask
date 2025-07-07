5c26418aab39c550dfeeb29e0c61db6ddd6d9479
Maxim Kastorsky <104034829+Olsney@users.noreply.github.com>
2025-07-07 17:04:52 +0300

    Updated dependencies bindings

--- Assets/Code/Infrastructure/Installers/BootstrapInstaller.cs
+++ Assets/Code/Infrastructure/Installers/BootstrapInstaller.cs
@@ -1,3 +1,7 @@
+using Code.Infrastructure.Factory;
+using Code.Infrastructure.States;
+using Code.Services.StaticData;
+using UnityEngine;
 using Zenject;
 
 namespace Code.Infrastructure.Installers
@@ -6,7 +10,42 @@ namespace Code.Infrastructure.Installers
     {
         public override void InstallBindings()
         {
+            BindCoroutine();
+            BindFactories();
+            BindStates();
+            BindServices();
+        }
+
+        private void BindCoroutine() => 
+            Container.Bind<ICoroutineRunner>().FromInstance(this).AsSingle();
+
+        private void BindFactories()
+        {
+            Container.Bind<IGameFactory>().To<GameFactory>().AsSingle();
+            Container.Bind<IStateFactory>().To<StateFactory>().AsSingle();
+        }
+
+        private void BindStates()
+        {
+            Container.Bind<GameStateMachine>().AsSingle();
+            Container.Bind<BootstrapState>().AsSingle();
+            Container.Bind<LoadLevelState>().AsSingle();
+            Container.Bind<LoadProgressState>().AsSingle();
+            Container.Bind<GameLoopState>().AsSingle();
+        }
         
+        private void BindServices()
+        {
+            BindInputService();
+            Container.Bind<IStaticDataService>().To<StaticDataService>().AsSingle();
+        }
+        
+        private void BindInputService()
+        {
+            if (Application.isEditor)
+                Container.Bind<IInputService>().To<StandaloneInputService>().AsSingle();
+            else
+                Container.Bind<IInputService>().To<MobileInputService>().AsSingle();
         }
     }
 }
