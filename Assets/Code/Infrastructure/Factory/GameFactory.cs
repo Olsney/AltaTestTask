fdc4b52506a183b7942f6bc079a8c6f11a3dbd4e
Maxim Kastorsky <104034829+Olsney@users.noreply.github.com>
2025-07-07 18:13:54 +0300

    Added logic of player ball creating

--- Assets/Code/Infrastructure/Factory/GameFactory.cs
+++ Assets/Code/Infrastructure/Factory/GameFactory.cs
@@ -1,12 +1,25 @@
+using Code.Infrastructure.AssetManagement;
 using UnityEngine;
+using Zenject;
 
 namespace Code.Infrastructure.Factory
 {
     public class GameFactory : IGameFactory
     {
+        private readonly IInstantiator _instantiator;
+        private readonly IAssetProvider _assets;
+
+        public GameFactory(IInstantiator instantiator, IAssetProvider assets)
+        {
+            _instantiator = instantiator;
+            _assets = assets;
+        }
+        
         public GameObject CreatePlayerBall(Vector3 at)
         {
-            return null;
+            GameObject prefab = _assets.Load(AssetPath.PlayerBallPath);
+            
+            return _instantiator.InstantiatePrefab(prefab, at, Quaternion.identity, null);
         }
     }
 }
