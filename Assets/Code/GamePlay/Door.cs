b1e540ca77d8c9cdee07be495a25f1d8f5b85579
Maxim Kastorsky <104034829+Olsney@users.noreply.github.com>
2025-07-08 21:06:33 +0300

    Added door open animation

--- Assets/Code/GamePlay/Door.cs
+++ Assets/Code/GamePlay/Door.cs
@@ -1,5 +1,6 @@
 using System;
 using Code.GamePlay.PlayerBall;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Code.GamePlay
@@ -7,23 +8,39 @@ namespace Code.GamePlay
     public class Door : MonoBehaviour
     {
         private const float RadiusToOpen = 5f;
+        private const float OpenHeight = 10f;
+        private const float OpenDuration = 1f;
+
+        private bool _isOpened;
 
         private void Update()
+        {
+            if (_isOpened)
+                return;
+
+            TryFindBall();
+        }
+
+        private void TryFindBall()
         {
             Collider[] colliders = Physics.OverlapSphere(transform.position, RadiusToOpen);
 
             foreach (Collider collider in colliders)
             {
-                if (collider.TryGetComponent(out Ball ball) == false)
+                if (collider.TryGetComponent(out Ball _) == false)
                     continue;
 
                 Open();
+                break;
             }
         }
 
         private void Open()
         {
-            
+            _isOpened = true;
+
+            transform.DOMoveY(transform.position.y + OpenHeight, OpenDuration)
+                .SetEase(Ease.OutCubic);
         }
     }
 }
