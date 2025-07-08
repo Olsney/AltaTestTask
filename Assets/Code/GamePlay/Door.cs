836b955245e681087a092b9b4918bfb9a910c83a
Maxim Kastorsky <104034829+Olsney@users.noreply.github.com>
2025-07-08 21:01:45 +0300

    Installed DOTween

--- Assets/Code/GamePlay/Door.cs
+++ Assets/Code/GamePlay/Door.cs
@@ -1,8 +1,29 @@
+using System;
+using Code.GamePlay.PlayerBall;
 using UnityEngine;
 
 namespace Code.GamePlay
 {
     public class Door : MonoBehaviour
     {
+        private const float RadiusToOpen = 5f;
+
+        private void Update()
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, RadiusToOpen);
+
+            foreach (Collider collider in colliders)
+            {
+                if (collider.TryGetComponent(out Ball ball) == false)
+                    continue;
+
+                Open();
+            }
+        }
+
+        private void Open()
+        {
+            
+        }
     }
 }
